6110e98697488b5f2d6f759bb6e71556f1684f2f
Ian Okumu <volunteeapp@gmail.com>
2021-02-25 12:52:47 +0300

    Lesson 1 - Introduction to Layouts

---
+++ Layouts/app/src/main/java/ke/ac/zalegoacademy/layouts/HomePage.kt
@@ -0,0 +1,11 @@
+package ke.ac.zalegoacademy.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
